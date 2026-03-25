bf9edef3e180194604cd508335c1e869898ef958
Dorian4536 <jb2034360@gmail.com>
2026-03-25 11:58:36 +0100

    ajusted REAME.md space problem

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
-const balance = document.getElementById("balance");
-const income = document.getElementById("income");
-const expense = document.getElementById("expense");
-const list = document.getElementById("list");
-const form = document.getElementById("form");
-const text = document.getElementById("text");
-const amount = document.getElementById("amount");
+const balance = document.getElementById('balance')
+const income = document.getElementById('income')
+const expense = document.getElementById('expense')
+const list = document.getElementById('list')
+const form = document.getElementById('form')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
 
-let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+let transactions = JSON.parse(localStorage.getItem('transactions')) || []
 
-function addTransaction(e) {
-  e.preventDefault();
+function addTransaction (e) {
+  e.preventDefault()
 
   const transaction = {
     id: Date.now(),
     text: text.value,
-    amount: +amount.value,
-  };
+    amount: +amount.value
+  }
 
-  transactions.push(transaction);
-  updateLocalStorage();
-  render();
+  transactions.push(transaction)
+  updateLocalStorage()
+  render()
 
-  text.value = "";
-  amount.value = "";
+  text.value = ''
+  amount.value = ''
 }
 
-function deleteTransaction(id) {
-  transactions = transactions.filter(t => t.id !== id);
-  updateLocalStorage();
-  render();
+function deleteTransaction (id) {
+  transactions = transactions.filter(t => t.id !== id)
+  updateLocalStorage()
+  render()
 }
 
-function render() {
-  list.innerHTML = "";
+function render () {
+  list.innerHTML = ''
 
-  let total = 0;
-  let incomeTotal = 0;
-  let expenseTotal = 0;
+  let total = 0
+  let incomeTotal = 0
+  let expenseTotal = 0
 
   transactions.forEach(t => {
-    const li = document.createElement("li");
-    li.classList.add(t.amount > 0 ? "plus" : "minus");
+    const li = document.createElement('li')
+    li.classList.add(t.amount > 0 ? 'plus' : 'minus')
 
     li.innerHTML = `
       ${t.text} <span>${t.amount} FCFA</span>
       <button class="delete-btn" onclick="deleteTransaction(${t.id})">x</button>
-    `;
+    `
 
-    list.appendChild(li);
+    list.appendChild(li)
 
-    total += t.amount;
+    total += t.amount
     if (t.amount > 0) {
-      incomeTotal += t.amount;
+      incomeTotal += t.amount
     } else {
-      expenseTotal += t.amount;
+      expenseTotal += t.amount
     }
-  });
+  })
 
-  balance.textContent = `${total} FCFA`;
-  income.textContent = `+${incomeTotal}`;
-  expense.textContent = `${expenseTotal}`;
+  balance.textContent = `${total} FCFA`
+  income.textContent = `+${incomeTotal}`
+  expense.textContent = `${expenseTotal}`
 }
 
-function updateLocalStorage() {
-  localStorage.setItem("transactions", JSON.stringify(transactions));
+function updateLocalStorage () {
+  localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
-form.addEventListener("submit", addTransaction);
+form.addEventListener('submit', addTransaction)
 
-render();
+render()
